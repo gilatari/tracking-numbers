1b87d039a149f42bb0ec0c0fc93ca0d4f7c3602e
gilatari <36481590+gilatari@users.noreply.github.com>
2020-01-22 23:31:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const EasyPost = require('@easypost/api');
 const api = new EasyPost('EZAK1ed12069664442f2a6985c190148af0aYZ0DxnsAvRUApZtR4nstVQ');
 
 const server = http.createServer((req, res) => {
-
+ 
+  res.header("Access-Control-Allow-Origin", "*");
   res.setHeader('Content-Type', 'application/json');
   var tracking_code = req.url.split('/tracking?code=');
 
   if(req.url.includes('/tracking?code=') && tracking_code.length > 0)
   {
- 
+
     res.statusCode = 200;
 
     const tracker = new api.Tracker({
